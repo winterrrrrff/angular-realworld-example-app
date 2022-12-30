a9174e9ae95d11bfd0dc2468c9b1f0323d662f36
Haofeng Feng <fenghaofeng@live.com>
2022-12-29 18:33:06 -0800

    fix import issue

--- src/app/article/markdown.pipe.ts
+++ src/app/article/markdown.pipe.ts
@@ -1,9 +1,10 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import * as marked from 'marked';
+import { marked } from 'marked';
 
 @Pipe({name: 'markdown'})
 export class MarkdownPipe implements PipeTransform {
   transform(content: string): string {
+    // @ts-ignore
     return marked(content, { sanitize: true });
   }
 }
